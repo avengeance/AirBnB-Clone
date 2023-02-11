78c155630252de78d62e8c4c89554c100a9a4cfe
Calvin Le <email.calvinle@gmail.com>
2023-02-11 15:07:04 -0700

    added seeds for spots

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -61,7 +61,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       User.hasMany(models.Review, { foreignKey: 'userId' })
       User.hasMany(models.Booking, { foreignKey: 'userId' })
-      User.hasMany(models.Spot, { foreignKey: 'userId' })
+      User.hasMany(models.Spot, { foreignKey: 'ownerId' })
     }
   }
   User.init(
---
+++ authenticate-me/backend/db/seeders/20230211214737-demo-spot.js
@@ -0,0 +1,55 @@
+'use strict';
+
+let options = {}
+if (process.enc.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    options.tableName = 'Spots'
+    return queryInterface.bulkInsert(options, [
+      {
+        address: '123 fake st',
+        city: 'Los Angeles',
+        state: 'California',
+        country: 'United States',
+        lat: 34.052235,
+        lng: -118.243683,
+        name: 'Mansion',
+        description: 'A big mansion',
+        price: 500.00,
+      }, {
+        address: '345 false cir',
+        city: 'New York',
+        state: 'New York',
+        country: 'United States',
+        lat: 40.71276,
+        lng: -74.005974,
+        name: 'Medium sized house',
+        description: 'A nice house in the big city',
+        price: 250.00,
+      }, {
+        address: '987 invalid ave',
+        city: 'Austin',
+        state: 'Texas',
+        country: 'United States',
+        lat: 30.267153,
+        lng: -97.743057,
+        name: 'Small house',
+        description: 'A small house to gather and explore the city',
+        price: 125.00,
+      }
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+
+    options.tableName = 'Spots'
+    const Op = Sequelize.Op
+    return queryInterface.bulkDelete(options, {
+      adress: { [Op.in]: ['123 fake st', '345 false cir', '987 invalid ave'] }
+    }, {})
+  }
+};
