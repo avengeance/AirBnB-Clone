3ab0bc8c6d9d76a256be6b3ae2ebeafbf0863205
Calvin Le <email.calvinle@gmail.com>
2023-02-11 12:52:10 -0700

    added associations to booking and review

--- authenticate-me/backend/db/models/booking.js
+++ authenticate-me/backend/db/models/booking.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      
+      Booking.belongsTo(models.Spot, { foreignKey: 'spotId' })
+      Booking.belongsTo(models.User, { foreignKey: 'userId' })
     }
   }
   Booking.init({
--- authenticate-me/backend/db/models/review.js
+++ authenticate-me/backend/db/models/review.js
@@ -10,7 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Review.belongsTo(models.User, { foreignKey: 'userId' })
+      Review.belongsTo(models.Spot, { foreignKey: 'spotId' })
+      Review.hasMany(models.ReviewImage, { foreignKey: 'reviewId' })
     }
   }
   Review.init({
