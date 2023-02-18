f13eada656ac86195e610aa599acf243a6aa6dd3
Calvin Le <email.calvinle@gmail.com>
2023-02-17 21:16:16 -0700

    complete get spots by current

--- authenticate-me/backend/routes/api/spots.js
+++ authenticate-me/backend/routes/api/spots.js
@@ -3,10 +3,13 @@ const express = require('express')
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Spot } = require('../../db/models');
 const { Review } = require('../../db/models');
+const { SpotImage } = require('../../db/models')
+const { User } = require('../../db/models')
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { QueryInterface } = require('sequelize');
+const spot = require('../../db/models/spot');
 
 const router = express.Router();
 
@@ -35,9 +38,34 @@ router.get('/', async (req, res) => {
 
 // Get all spots by the current user
 
-router.get('/current', async (req, res) => {
-    const user = await Spot.findAll()
-    return res.json({ user })
+router.get('/current', requireAuth, async (req, res) => {
+    const userId = req.user.id
+    const user = await Spot.findByPk(userId)
+    if (user) {
+        allSpots = await Spot.findAll({ where: { ownerId: userId } })
+        res.status(200)
+        return res.json({ "Spots": allSpots })
+    } else {
+        throw new Error('No user logged in')
+    }
+
+})
+
+// Get details of a Spot from an Id
+// server is hanging
+router.get('/:spotid', async (req, res) => {
+    const spotId = req.params.id
+    const spot = await Spot.findByPk(spotId, {
+        include: [
+            { model: SpotImage, attributes: ['id', 'url', 'preview'] },
+            { model: User, as: 'Owner', attributes: ['id', 'firstName', 'lastName'] }
+        ]
+    })
+
+    if (spot) {
+        res.status(200)
+        return res.json(spot)
+    }
 })
 
 module.exports = router;
