4f532298f605795a6c7ce33b2f8e547117a884e1
Calvin Le <email.calvinle@gmail.com>
2023-02-20 19:43:57 -0700

    edit review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -6,11 +6,21 @@ const { Spot, Review, SpotImage, User, Booking, ReviewImage } = require('../../d
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { QueryInterface, Sequelize } = require('sequelize');
-const spotimage = require('../../db/models/spotimage');
-const reviewimage = require('../../db/models/reviewimage');
 
 const router = express.Router();
 
+const reviewValidationError = [
+    check('review')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Review text is required'),
+    check('stars')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .isFloat({ min: 1, max: 5 })
+        .withMessage('Stars must be an integer from 1 to 5'),
+    handleValidationErrors
+]
 
 // Get all reviews of the current user
 // find a way to remove 'SpotImages' and only display previewImage
@@ -31,21 +41,21 @@ router.post('/:reviewId/images', requireAuth, async (req, res) => {
     const reviewId = req.params.reviewId
     const { url } = req.body
     const review = await Review.findByPk(reviewId)
-    const countReview = await Review.findByPk(reviewId, {
-        attributes: [
-            [Sequelize.fn('COUNT', Sequelize.col('ReviewImage.id')), 'numReviews']
-        ],
-        include: [
-            { model: reviewimage, attributes: [] }
-        ]
-    })
+    // const countReview = await Review.findByPk(reviewId, {
+    //     attributes: [
+    //         [Sequelize.fn('COUNT', Sequelize.col('ReviewImage.id')), 'numReviews']
+    //     ],
+    //     include: [
+    //         { model: reviewimage, attributes: [] }
+    //     ]
+    // })
 
-    if (countReview >= 10) {
-        return res.status(403).json({
-            "message": "Maximum number of images for this resource was reached",
-            "statusCode": 403
-        })
-    }
+    // if (countReview >= 10) {
+    //     return res.status(403).json({
+    //         "message": "Maximum number of images for this resource was reached",
+    //         "statusCode": 403
+    //     })
+    // }
     if (!review) {
         return res.status(404).json({
             "message": "Review couldn't be found",
@@ -60,6 +70,32 @@ router.post('/:reviewId/images', requireAuth, async (req, res) => {
         id: newImage.id,
         url: newImage.url
     })
+})
+
+// Edit a review
+router.put('/:reviewId', requireAuth, reviewValidationError, async (req, res) => {
+    const reviewId = req.params.reviewId
+    const updateReview = await Review.findByPk(reviewId)
+    const userId = req.user.id
+    const { review, stars } = req.body
+    if (!updateReview) {
+        return res.status(404).json({
+            "message": "Review couldn't be found",
+            "statusCode": 404
+        })
+    }
+    // if (review.userId !== userId) {
+    //     res.status(400).json({
+    //         "message": "User not authorized"
+    //     })
+    // }
+    const update = await review.update({
+        review,
+        stars,
+    });
+
+
+    return res.status(200).json(update)
 })
 
 module.exports = router;
