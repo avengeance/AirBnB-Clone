448c50da3c3dafc46ce087d432ae0671148c7f2d
Calvin Le <email.calvinle@gmail.com>
2023-02-21 18:56:08 -0700

    bookings commit

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -10,20 +10,25 @@ const { QueryInterface, Sequelize } = require('sequelize');
 const router = express.Router();
 
 router.get('/current', requireAuth, async (req, res) => {
+    const { SpotImage } = require('./index.js')
     const allBookings = await Booking.findAll({
         // attributes: ['id', 'spotId'],
-        attributes: ['id', 'spotId', 'userId', 'startDate', 'endDate', 'createdAt', 'updatedAt'],
-        include: [
-            {
-                model: Spot, attributes: [
-                    'id', 'ownerId', 'address', 'city', 'state', 'country',
-                    'lat', 'lng', 'name', 'description', 'price',
-                ]
-            }
-        ],
+        attributes: ['id', 'spotId', {
+            include: [
+                {
+                    model: Spot, attributes: [
+                        'id', 'ownerId', 'address', 'city', 'state', 'country',
+                        'lat', 'lng', 'name', 'description', 'price',
+                        // include: { model: SpotImage, attributes: [] },
+                        // include: [Sequelize.col('SpotImages.url'), 'previewImage',]
+                        // }
+                    ]
+                }
+            ]
+        }, 'userId', 'startDate', 'endDate', 'createdAt', 'updatedAt'],
     },
-    )
-    return res.json({ "Bookings": allBookings })
+)
+return res.json({ "Bookings": allBookings })
 })
 
 module.exports = router;
