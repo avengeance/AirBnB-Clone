0af17056ed277f5a6b0e98e5e8727fd0b7a59a3c
Calvin Le <email.calvinle@gmail.com>
2023-03-30 20:16:09 -0700

    added some console logs having issues still

--- frontend/src/components/Spots/SpotDetail.js
+++ frontend/src/components/Spots/SpotDetail.js
@@ -4,20 +4,40 @@ import { useParams } from "react-router-dom";
 function SpotDetail() {
   const { spotId } = useParams();
 
-  const [spot, setSpot] = useState(null);
+  const [spot, setSpot] = useState({
+    id: "",
+    ownerId: '',
+    address: "",
+    city: "",
+    state: "",
+    country: "",
+    imageUrl: "",
+    description: "",
+    Owner: { firstName: "", lastName: "" },
+  });
 
   useEffect(() => {
     fetch(`/api/spots/${spotId}`)
       .then((res) => res.json())
       .then((data) => {
-        setSpot(data.spot);
+        if (data.spot !== undefined) {
+          setSpot(data.spot);
+        }
+        console.log('data: ', data);
+        console.log('data.spot: ', data.spot);
+        console.log('spot: ', spot);
+        console.log('spotId:', spotId)
       });
   }, [spotId]);
 
+  useEffect(() => {
+    console.log('spot:', spot);
+  }, [spot]);
+
   return (
     <div>
-      {spot ? (
-        <div>
+      {spot && spot.id ? (
+        <div className="spot-name-loc-detail">
           <h2>{spot.name}</h2>
           <h3>
             Location: {spot.city}, {spot.state}, {spot.country}
