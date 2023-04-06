8d253b55ac020cb87103f354e05755ef71ec8738
Calvin Le <email.calvinle@gmail.com>
2023-04-05 20:35:56 -0700

    changed back tiks to apostrophe

--- backend/db/seeders/20230212224639-demo-review.js
+++ backend/db/seeders/20230212224639-demo-review.js
@@ -31,93 +31,45 @@ module.exports = {
       {
         userId: 2,
         spotId: 1,
-        review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-       nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-       qui officia deserunt mollit anim id est laborum.`,
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         stars: 3
       },
       {
         userId: 3,
         spotId: 1,
-        review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-       nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-       qui officia deserunt mollit anim id est laborum.`,
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         stars: 5
       },
       {
         userId: 1,
         spotId: 2,
-        review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-       nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-       qui officia deserunt mollit anim id est laborum.`,
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         stars: 3
       }, {
         userId: 2,
         spotId: 2,
-        review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-       nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-       qui officia deserunt mollit anim id est laborum.`,
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         stars: 2
       }, {
         userId: 3,
         spotId: 2,
-        review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-       nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-       qui officia deserunt mollit anim id est laborum.`,
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         stars: 1
       },
       {
         userId: 3,
         spotId: 3,
-        review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-       nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-       qui officia deserunt mollit anim id est laborum.`,
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         stars: 2
       }, {
         userId: 2,
         spotId: 3,
-        review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-       nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-       qui officia deserunt mollit anim id est laborum.`,
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         stars: 5
       }, {
         userId: 1,
         spotId: 3,
-        review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-       nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-       qui officia deserunt mollit anim id est laborum.`,
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         stars: 5
       },
     ])
